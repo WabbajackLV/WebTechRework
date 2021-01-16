f4f251c0ca1f77fc3c17fcf7883053dcb84beadf
kraft <kraft@DESKTOP-13IH11N>
2021-01-16 19:52:08 +0200

    Created Header component

---
+++ cafe/public/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header(){
+    return (<header>
+        <div>
+            {/* Header navigation buttons */}
+            <a  href="">Home</a>    
+            <a  href="">Menu</a>
+            <a  href="">About Us</a>
+            <a  href="">Login</a>
+        </div>
+    </header>);
+}
+
+export default Header;
