c21ae36eb4806e2d9fec6a22aa0c84cd52607bd6
kraft <kraft@DESKTOP-13IH11N>
2021-01-16 19:53:16 +0200

    Created Main component

---
+++ cafe/public/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Section from './Section';
+import Section2 from './Section2';
+
+function Main(){
+    return (<div>
+        {/* Sections provide with content for the main page */}
+        <Section />
+        <Section2 />
+    </div>);
+}
+
+export default Main;
