03695e7d55979bc83edd84e26c43caf62fbee8af
kraft <kraft@DESKTOP-13IH11N>
2021-01-17 20:21:53 +0200

    Made changes to nav bar

--- cafe/src/components/App.jsx
+++ cafe/src/components/App.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import Header from './Header';
 import Main from './Main';
 import stylesheet from '../styles.scss';
+import AboutUs from './Us';
+
+var state = {visible: true}
 
 function App(){
-    return (<div>
-    <Header />
-    <Main />
-    </div>);
+    return (
+        <div>
+            <nav>
+                <button>Home</button>
+                <button>Menu</button>
+                <button>Order</button>
+                <button>About Us</button>
+            </nav>
+            <Main />
+        </div>
+    );
 }
 
 export default App;
