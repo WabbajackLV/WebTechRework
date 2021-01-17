fde4e5cf4801c72ad5d83bfb9a4d7df7a4438fc1
kraft <kraft@DESKTOP-13IH11N>
2021-01-17 21:49:54 +0200

    adjusted minor issues with aboutus view

--- cafe/src/components/Footer.jsx
+++ cafe/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer(){
     const currentYear = new Date().getFullYear();
 
-    return (<footer className="aboutUsFooter">
+    return (<footer>
         <p>Copyright &copy; {currentYear} Arvis Krafts</p>
     </footer>);
 }
